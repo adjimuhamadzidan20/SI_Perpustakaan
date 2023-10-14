df44faaad62b1f606a716c18aa4c5c58f3ffeef8
adjimuhamadzidan20 <adjimuhamadzidan@gmail.com>
2023-10-14 20:03:35 +0700

    perbaikan tampilan report masing file

--- app/Views/cetak_laporan/cetak_penerbit.php
+++ app/Views/cetak_laporan/cetak_penerbit.php
@@ -1,7 +1,17 @@
+<?php  
+  setlocale(LC_ALL, 'id-ID', 'id_ID');
+  $tgl1 = strftime("%A, %d %B %Y | %T");
+  $tgl2 = strftime("%d %B %Y");
+?>
+
 <body>
   <div style="width: 80%; margin: auto;">
+    <div class="card-header" style="border-bottom: 2px solid black; padding-bottom: 5px;">
+      <h1>SI Perpustakaan</h1>    
+    </div>
     <div class="card-body">
-      <h4 class="mb-3">Laporan Data Penerbit Perpustakaan</h4>  
+      <h5 class="mb-2">Laporan Data Penerbit Perpustakaan</h5>
+      <p class="mb-3"><?= $tgl1; ?></p>  
       <table class="table table-bordered">
         <thead>
           <tr>
@@ -26,6 +36,12 @@
       </table>
     </div>
     <!-- /.card-body -->
+    <div class="card-footer" style="text-align: right; margin-top:40px; background-color: white;">
+      <p>Kota Bekasi, <?= $tgl2; ?></p>
+      <p style="margin-right: 25px;">Admin Perpustakaan</p>
+      <br><br>
+      <p style="margin-right: 27px;">..................................</p>
+    </div>
   </div>
 </body>
 
